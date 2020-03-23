09a67d5e0d231cb197c94bf121a2a3d91b43e23f
TheEzo <willaschek.t@gmail.com>
2020-03-23 22:39:37 +0100

    backup

--- test.sh
+++ test.sh
@@ -9,20 +9,33 @@ then
     exit 1
   fi
 else
-    numbers=$1
-fi;
+  numbers=$1
+fi
 
 mpic++ --prefix /usr/local/share/OpenMPI -o oets odd-even.cpp
 
 if [ $test -eq 0 ]
 then
- dd if=/dev/random bs=1 count=$numbers of=numbers
+  dd if=/dev/random bs=1 count=$numbers of=numbers status=none
+  mpirun --prefix /usr/local/share/OpenMPI -np $numbers oets
 else
-
+  rm testfiles/*
+  for i in {4..30}
+  do
+    printf $i >> output
+    for j in {1..50}
+    do
+      dd if=/dev/random bs=1 count=$i of=testfiles/$i iflag=append conv=notrunc status=none
+      mv testfiles/$i numbers
+      printf ";" >> output
+      mpirun --prefix /usr/local/share/OpenMPI -np $i oets
+      printf $? >> output
+      mv numbers testfiles/$i
+    done
+    printf "\n" >> output
+  done
 fi
 
-#vyrobeni souboru s random cisly
-dd if=/dev/random bs=1 count=$numbers of=numbers
 
 #spusteni
 mpirun --prefix /usr/local/share/OpenMPI -np $numbers oets
